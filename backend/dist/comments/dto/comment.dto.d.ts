a9af9ba38eca87878500b5cf043cd8e8ffe2385b
myls <137297604+marnonalee@users.noreply.github.com>
2025-10-24 22:53:14 +0800

    Delete backend/dist directory

--- backend/dist/comments/dto/comment.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateCommentDto {
-    content: string;
-    postId: number;
-}
-declare const UpdateCommentDto_base: import("@nestjs/common").Type<Partial<CreateCommentDto>>;
-export declare class UpdateCommentDto extends UpdateCommentDto_base {
-    content?: string;
-}
-export {};
